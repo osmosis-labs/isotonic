a02df99639a586e934a462465ef4f7f49605bb82
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-12-08 10:06:08 +0100

    lendex-token: Transfer execution

--- contracts/lendex-token/src/contract.rs
+++ contracts/lendex-token/src/contract.rs
@@ -1,13 +1,13 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128,
+    to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128,
 };
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{TokenInfo, TOKEN_INFO};
+use crate::msg::{CanTransferResp, ControllerQuery, ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{TokenInfo, BALANCES, TOKEN_INFO, TOTAL_SUPPLY};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:lendex-token";
@@ -34,14 +34,88 @@ pub fn instantiate(
     Ok(Response::new())
 }
 
+fn can_transfer(
+    deps: Deps,
+    env: &Env,
+    account: String,
+    amount: Uint128,
+) -> Result<(), ContractError> {
+    let token_info = TOKEN_INFO.load(deps.storage)?;
+    let can_transfer: CanTransferResp = deps.querier.query_wasm_smart(
+        token_info.controller,
+        &ControllerQuery::CanTransfer {
+            token: env.contract.address.to_string(),
+            account,
+            amount,
+        },
+    )?;
+
+    match can_transfer {
+        CanTransferResp::None => Err(ContractError::CannotTransfer {
+            max_transferable: Uint128::zero(),
+        }),
+        CanTransferResp::Partial(max_transferable) => {
+            Err(ContractError::CannotTransfer { max_transferable })
+        }
+        CanTransferResp::Whole => Ok(()),
+    }
+}
+
+fn transfer(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    recipient: String,
+    amount: Uint128,
+) -> Result<Response, ContractError> {
+    if amount == Uint128::zero() {
+        return Err(ContractError::InvalidZeroAmount {});
+    }
+
+    can_transfer(deps.as_ref(), &env, info.sender.to_string(), amount)?;
+    // Not validating recipient, as if it is invalid one, the transfer
+    // would be refused by controller.
+    let recipient = Addr::unchecked(&recipient);
+
+    BALANCES.update(
+        deps.storage,
+        &info.sender,
+        |balance: Option<Uint128>| -> StdResult<_> {
+            balance
+                .unwrap_or_default()
+                .checked_sub(amount)
+                .map_err(Into::into)
+        },
+    )?;
+
+    BALANCES.update(
+        deps.storage,
+        &recipient,
+        |balance: Option<Uint128>| -> StdResult<_> { Ok(balance.unwrap_or_default() + amount) },
+    )?;
+
+    let res = Response::new()
+        .add_attribute("action", "transfer")
+        .add_attribute("from", info.sender)
+        .add_attribute("to", recipient)
+        .add_attribute("amount", amount);
+
+    Ok(res)
+}
+
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    todo!()
+    use ExecuteMsg::*;
+
+    match msg {
+        Transfer { recipient, amount } => transfer(deps, env, info, recipient, amount),
+        _ => todo!(),
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
