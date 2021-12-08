79d7dd8d3bfc104ab5c1c399a9092bb94a914b2f
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-12-08 09:29:16 +0100

    lendex-token: Messages set up

--- contracts/lendex-token/src/msg.rs
+++ contracts/lendex-token/src/msg.rs
@@ -1,27 +1,69 @@
+use cosmwasm_std::Uint128;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    /// Token name
+    pub name: String,
+    /// Token symbol
+    pub symbol: String,
+    /// Token precision for displaying
+    pub decimals: u8,
+    /// Controller is contract allowed to ming, burn, rebase, and must be checked with to
+    /// enable transfer
+    pub controller: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    /// Transfer is a base message to move tokens to another account without triggering actions.
+    /// Requires check for transfer possibility by `ControllerQuery::CanTransfer` call to
+    /// controller.
+    Transfer { recipient: String, amount: Uint128 },
+    /// Reserved for controller
+    Burn { amount: Uint128 },
+    /// Send is a base message to transfer tokens to a contract and trigger an action
+    /// on the receiving contract.
+    /// Requires check for transfer possibility by `ControllerQuery::CanTransfer` call to
+    /// controller.
+    Send {
+        contract: String,
+        amount: Uint128,
+        msg: Binary,
+    },
+    /// Reserved for controller
+    Mint { recipient: String, amount: Uint128 },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum ControllerQuery {
+    CanTransfer {
+        /// Lendex contract address that calls "CanTransfer"
+        token: String,
+        /// Address that wishes to transfer
+        account: String,
+        /// The amount we wish to transfer from theri account
+        amount: Uint128,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    /// Returns the current balance of the given address, 0 if unset.
+    /// Return type: BalanceResponse.
+    Balance { address: String },
+    /// Returns metadata on the contract - name, decimals, supply, etc.
+    /// Return type: TokenInfoResponse.
+    TokenInfo {},
 }
 
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct TokenInfoResponse {
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub total_supply: Uint128,
 }
