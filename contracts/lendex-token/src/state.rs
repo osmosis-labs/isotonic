c29f9649364ead1e528a08468db4b0b6013050ae
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-12-08 09:35:00 +0100

    lendex-token: State set up

--- contracts/lendex-token/src/state.rs
+++ contracts/lendex-token/src/state.rs
@@ -1,13 +1,18 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+#[serde(rename_all = "snake_case")]
+pub struct TokenInfo {
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub total_supply: Uint128,
+    pub controller: Addr,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const TOKEN_INFO: Item<TokenInfo> = Item::new("token_info");
+pub const BALANCES: Map<&Addr, Uint128> = Map::new("balance");
