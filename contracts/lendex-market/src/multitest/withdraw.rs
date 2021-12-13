642115ba29b623fbca7c1bd930b58ea6a7acd94d
Tomasz Kurcz <uint@lavabit.com>
2021-12-13 17:36:18 +0100

    fix test comments

--- contracts/lendex-market/src/multitest/withdraw.rs
+++ contracts/lendex-market/src/multitest/withdraw.rs
@@ -33,13 +33,12 @@ fn withdraw_overflow_is_handled() {
     // Deposit some tokens so we have something to withdraw.
     suite.deposit(lender, &[Coin::new(100, "ATOM")]).unwrap();
 
-    // After the deposit, the lender has 100 l-token and the contract has 100 base asset.
-    // The lender should be able to withdraw 40 tokens.
+    // Try to withdraw more base asset than we have deposited - should fail and not
+    // affect any balances.
     assert_eq!(
         suite.withdraw(lender, 150).unwrap_err().to_string(),
         "Performing operation while there is not enough tokens, 100 tokens available, 150 needed"
     );
-
     assert_eq!(suite.query_asset_balance(lender).unwrap(), 0);
     assert_eq!(suite.query_contract_asset_balance().unwrap(), 100);
     assert_eq!(suite.query_ltoken_balance(lender).unwrap().u128(), 100);
