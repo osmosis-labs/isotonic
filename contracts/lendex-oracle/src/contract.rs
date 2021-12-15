46e931a4b9b2a8a6de790850eb6b0d38d4e80536
Tomasz Kurcz <uint@lavabit.com>
2021-12-15 20:27:05 +0100

    lendex-oracle: implement tests for basic features

--- contracts/lendex-oracle/src/contract.rs
+++ contracts/lendex-oracle/src/contract.rs
@@ -1,7 +1,7 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Decimal, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+    to_binary, Binary, Decimal, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult,
 };
 use cw2::set_contract_version;
 
@@ -62,7 +62,10 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     use QueryMsg::*;
     match msg {
         Configuration {} => to_binary(&CONFIG.load(deps.storage)?),
-        Price { sell, buy } => to_binary(&query::price(deps, &sell, &buy)?),
+        Price { sell, buy } => to_binary(
+            &query::price(deps, &sell, &buy)
+                .map_err(|err| StdError::generic_err(err.to_string()))?,
+        ),
     }
 }
 
@@ -71,7 +74,7 @@ mod query {
 
     use super::*;
 
-    pub fn price(_deps: Deps, _sell: &str, _buy: &str) -> StdResult<PriceResponse> {
+    pub fn price(_deps: Deps, _sell: &str, _buy: &str) -> Result<PriceResponse, ContractError> {
         todo!()
     }
 }
@@ -81,10 +84,10 @@ mod tests {
     use cosmwasm_std::{
         from_slice,
         testing::{mock_dependencies, mock_env, mock_info},
-        Addr,
+        Addr, StdError,
     };
 
-    use crate::time::Duration;
+    use crate::{msg::PriceResponse, time::Duration};
 
     use super::*;
 
@@ -95,6 +98,44 @@ mod tests {
         };
         let info = mock_info("creator", &[]);
         instantiate(deps, mock_env(), info, msg).unwrap();
+    }
+
+    fn set_price(
+        deps: DepsMut,
+        setter: &str,
+        sell: &str,
+        buy: &str,
+        rate: Decimal,
+    ) -> Result<Response, ContractError> {
+        execute(
+            deps,
+            mock_env(),
+            mock_info(setter, &[]),
+            ExecuteMsg::SetPrice {
+                sell: sell.to_string(),
+                buy: buy.to_string(),
+                rate,
+            },
+        )
+    }
+
+    fn query_price(deps: Deps, env: Env, sell: &str, buy: &str) -> Result<PriceResponse, StdError> {
+        let raw = query(
+            deps,
+            env,
+            QueryMsg::Price {
+                sell: sell.to_string(),
+                buy: buy.to_string(),
+            },
+        )
+        .unwrap();
+        from_slice(&raw)
+    }
+
+    fn mock_env_after_secs(secs: u64) -> Env {
+        let mut env = mock_env();
+        env.block.time = env.block.time.plus_seconds(secs);
+        env
     }
 
     #[test]
@@ -110,6 +151,73 @@ mod tests {
                 oracle: Addr::unchecked("oracle"),
                 maximum_age: Duration::new(333),
             }
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn set_and_query_price() {
+        let mut deps = mock_dependencies();
+        do_instantiate(deps.as_mut(), "oracle", 333);
+
+        // The oracle can set the price and we can later query it.
+        set_price(deps.as_mut(), "oracle", "ATOM", "BTC", Decimal::percent(60)).unwrap();
+
+        let res = query_price(deps.as_ref(), mock_env(), "ATOM", "BTC").unwrap();
+        assert_eq!(
+            res,
+            PriceResponse {
+                rate: Decimal::percent(60),
+            }
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn set_price_unauthorized() {
+        let mut deps = mock_dependencies();
+        do_instantiate(deps.as_mut(), "oracle", 333);
+
+        // "some_dude" isn't the address of the oracle here, so we throw
+        // an `Unauthorized` error
+        let resp = set_price(
+            deps.as_mut(),
+            "some_dude",
+            "ATOM",
+            "BTC",
+            Decimal::percent(60),
+        );
+        assert_eq!(resp, Err(ContractError::Unauthorized {}));
+    }
+
+    #[test]
+    #[ignore]
+    fn query_outdated_price() {
+        let mut deps = mock_dependencies();
+        do_instantiate(deps.as_mut(), "oracle", 333);
+
+        // The oracle can set the price and we can later query it.
+        set_price(deps.as_mut(), "oracle", "ATOM", "BTC", Decimal::percent(60)).unwrap();
+
+        // Query after the last record already expired.
+        let res = query_price(deps.as_ref(), mock_env_after_secs(355), "ATOM", "BTC").unwrap_err();
+        assert_eq!(
+            res,
+            StdError::generic_err("The prices for this trading pair are outdated")
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn query_nonexistent_price() {
+        let mut deps = mock_dependencies();
+        do_instantiate(deps.as_mut(), "oracle", 333);
+
+        // Query a trading pair that was never recorded
+        let res = query_price(deps.as_ref(), mock_env(), "ATOM", "BTC").unwrap_err();
+        assert_eq!(
+            res,
+            StdError::generic_err("There is no info about the prices for this trading pair")
         );
     }
 }
