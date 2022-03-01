f0b0d2814c3395ea7fd3a144cf77b1f7d8824254
Jakub Bogucki <jakub@confio.gmbh>
2022-03-01 10:36:48 +0100

    Market: add test for half year reserves

--- contracts/lendex-market/src/multitest/reserve.rs
+++ contracts/lendex-market/src/multitest/reserve.rs
@@ -56,3 +56,55 @@ fn reserve_factor_after_full_year() {
 
     assert_eq!(suite.query_reserve().unwrap(), Uint128::new(160));
 }
+
+#[test]
+fn reserve_factor_after_half_year() {
+    let lender = "lender";
+    let borrower = "borrower";
+    let market_token = "atom";
+    let mut suite = SuiteBuilder::new()
+        .with_funds(lender, &[coin(5000, market_token)])
+        .with_funds(borrower, &[coin(500, market_token)])
+        .with_interest(4, 20)
+        .with_reserve_factor(20)
+        .with_market_token(market_token)
+        .build();
+
+    // Set arbitrary market/common exchange ratio and credit lines (not part of this test)
+    suite.set_token_ratio_one().unwrap();
+    suite.set_high_credit_line(borrower).unwrap();
+    suite.set_high_credit_line(lender).unwrap();
+
+    // Deposit some tokens
+    suite
+        .deposit(lender, &[Coin::new(4000, market_token)])
+        .unwrap();
+
+    // Borrow some tokens
+    suite.borrow(borrower, 3000).unwrap();
+
+    suite.advance_seconds((SECONDS_IN_YEAR / 2) as u64);
+
+    // Deposit some tokens
+    // interests are 19% (4% base + 20% slope * 75% utilization)
+    // supplied (ltokens) = 4000
+    // borrwed (btokens) = 3000
+    // reserve = 20% * borrowed = 600
+    // bMul (btoken_ratio) = 10% after half year
+    // liquid assets = 1000
+    // ltokens supplied = 3000 + 1000 - 600 = 3400
+    // lMul (ltoken_ratio) = borrowed * bMul / lMul = 3000 * 0.1 / 3400 ~= 0.083
+    // that means ltokens 4000 * 1.083 = 4332
+    // deposit 1000 -> 5332 left btokens
+    suite
+        .deposit(lender, &[Coin::new(1000, market_token)])
+        .unwrap();
+
+    // TODO: rounding error
+    assert_eq!(
+        suite.query_ltoken_info().unwrap().total_supply,
+        DisplayAmount::raw(5334u128)
+    );
+
+    assert_eq!(suite.query_reserve().unwrap(), Uint128::new(600));
+}
