e3ca5bd26c21cffc41afe2df43d2458eec2025ce
Tomasz Kurcz <uint@lavabit.com>
2022-03-14 22:19:04 +0100

    clean out isotonic_osmosis_oracle

--- contracts/isotonic-osmosis-oracle/examples/schema.rs
+++ contracts/isotonic-osmosis-oracle/examples/schema.rs
@@ -3,8 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use isotonic_osmosis_oracle::msg::{ExecuteMsg, InstantiateMsg, PriceResponse, QueryMsg};
-use isotonic_osmosis_oracle::state::Config;
+use isotonic_osmosis_oracle::msg::{PriceResponse, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -12,9 +11,6 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(PriceResponse), &out_dir);
-    export_schema(&schema_for!(Config), &out_dir);
 }
--- contracts/isotonic-osmosis-oracle/src/error.rs
+++ contracts/isotonic-osmosis-oracle/src/error.rs
@@ -6,6 +6,9 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
+    #[error("This contract cannot be executed")]
+    NoExecute {},
+
     #[error("Unauthorized")]
     Unauthorized {},
 
