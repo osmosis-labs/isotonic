cfbe78b70cd54db03f42c042484718a9eede2c86
Tomasz Kurcz <uint@lavabit.com>
2022-03-15 14:20:49 +0100

    osmosis oracle: implementation

--- contracts/isotonic-osmosis-oracle/src/contract.rs
+++ contracts/isotonic-osmosis-oracle/src/contract.rs
@@ -1,23 +1,33 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response};
+use cosmwasm_std::{to_binary, Binary, Env, MessageInfo, Response};
 use cw2::set_contract_version;
+use osmo_bindings::OsmosisQuery;
 
 use crate::error::ContractError;
-use crate::msg::QueryMsg;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config, CONFIG};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:isotonic-oracle";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+type Deps<'a> = cosmwasm_std::Deps<'a, OsmosisQuery>;
+type DepsMut<'a> = cosmwasm_std::DepsMut<'a, OsmosisQuery>;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     info: MessageInfo,
-    _msg: Empty,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let cfg = Config {
+        controller: deps.api.addr_validate(&msg.controller)?,
+    };
+    CONFIG.save(deps.storage, &cfg)?;
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
@@ -27,12 +37,18 @@ pub fn instantiate(
 /// Execution entry point
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
-    _msg: Empty,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    Err(ContractError::NoExecute {})
+    match msg {
+        ExecuteMsg::AddPool {
+            pool_id,
+            denom1,
+            denom2,
+        } => exec::add_pool(deps, info, pool_id, &denom1, &denom2),
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -40,23 +56,78 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> Result<Binary, ContractErro
     use QueryMsg::*;
 
     let res = match msg {
-        Price { sell, buy } => to_binary(&query::price(deps, env, &sell, &buy)?)?,
+        Price { sell, buy } => to_binary(&query::price(deps, env, sell, buy)?)?,
     };
 
     Ok(res)
 }
 
+mod exec {
+    use cosmwasm_std::ensure_eq;
+
+    use crate::state::POOLS;
+
+    use super::*;
+
+    /// Handler for `ExecuteMsg::SetPrice`
+    pub fn add_pool(
+        deps: DepsMut,
+        info: MessageInfo,
+        pool_id: u64,
+        denom1: &str,
+        denom2: &str,
+    ) -> Result<Response, ContractError> {
+        let cfg = CONFIG.load(deps.storage)?;
+        ensure_eq!(info.sender, cfg.controller, ContractError::Unauthorized {});
+
+        let denoms = if denom1 < denom2 {
+            (denom1, denom2)
+        } else {
+            (denom2, denom1)
+        };
+        POOLS.save(deps.storage, denoms, &pool_id)?;
+
+        Ok(Response::new()
+            .add_attribute("action", "set_price")
+            .add_attribute("sender", info.sender))
+    }
+}
+
 mod query {
-    use crate::msg::PriceResponse;
+    use cosmwasm_std::QueryRequest;
+    use osmo_bindings::{SpotPriceResponse, Swap};
+
+    use crate::{msg::PriceResponse, state::POOLS};
 
     use super::*;
 
     pub fn price(
-        _deps: Deps,
+        deps: Deps,
         _env: Env,
-        _sell: &str,
-        _buy: &str,
+        sell: String,
+        buy: String,
     ) -> Result<PriceResponse, ContractError> {
-        todo!()
+        let denoms = if sell < buy {
+            (sell.as_str(), buy.as_str())
+        } else {
+            (buy.as_str(), sell.as_str())
+        };
+
+        let pool_id = POOLS
+            .may_load(deps.storage, denoms)?
+            .ok_or(ContractError::NoInfo {})?;
+
+        let price: SpotPriceResponse =
+            deps.querier
+                .query(&QueryRequest::Custom(OsmosisQuery::SpotPrice {
+                    swap: Swap {
+                        pool_id,
+                        denom_in: sell,
+                        denom_out: buy,
+                    },
+                    with_swap_fee: true,
+                }))?;
+
+        Ok(PriceResponse { rate: price.price })
     }
 }
--- contracts/isotonic-osmosis-oracle/src/msg.rs
+++ contracts/isotonic-osmosis-oracle/src/msg.rs
@@ -3,6 +3,22 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub controller: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    /// Add an Osmosis liquidity pool to the list. Only callable by the controller
+    AddPool {
+        pool_id: u64,
+        denom1: String,
+        denom2: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     /// Queries the exchange rate between two denoms
