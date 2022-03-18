4b521beac864c05fa72b217052ac4b2c82f9efe1
Tomasz Kurcz <uint@lavabit.com>
2022-03-18 13:42:57 +0100

    osmosis_oracle: add missing exports

--- contracts/isotonic-osmosis-oracle/examples/schema.rs
+++ contracts/isotonic-osmosis-oracle/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use isotonic_osmosis_oracle::msg::{PriceResponse, QueryMsg};
+use isotonic_osmosis_oracle::msg::{ExecuteMsg, InstantiateMsg, PriceResponse, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -11,6 +11,8 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(PriceResponse), &out_dir);
 }
