a73e917f57d6803a77b3fb9621395d7d6e2f5a81
Tomasz Kurcz <uint@lavabit.com>
2022-04-21 10:34:22 +0200

    tests: doc comments

--- tests/tests/acceptance.rs
+++ tests/tests/acceptance.rs
@@ -1,4 +1,11 @@
 //! https://confio.slab.com/posts/acceptance-tests-q7uuk5oi
+//!
+//! These tests assume that 1 unit of a token is represented by 1_000_000.
+//! This way we can verify the fractions mentioned in the slab document.
+//!
+//! User A = Alice
+//! User B = Bob
+//! User C = Carol
 
 use cosmwasm_std::{coin, Decimal, Uint128};
 
@@ -11,26 +18,29 @@ fn liquidate_via_amm() {
     let mut suite = SuiteBuilder::new()
         .with_market(MarketBuilder::new("A").with_collateral_ratio(Decimal::percent(65)))
         .with_market(MarketBuilder::new("B").with_collateral_ratio(Decimal::percent(65)))
-        .with_funds("alice", &[coin(100_000_000, "A")])
-        .with_funds("bob", &[coin(100_000_000, "A"), coin(100_000_000, "B")])
+        .with_funds("alice", &[coin(100_000_000_000, "A")])
+        .with_funds(
+            "bob",
+            &[coin(100_000_000_000, "A"), coin(100_000_000_000, "B")],
+        )
         .with_common_token("C")
-        .with_pool(1, (coin(100_000_000, "A"), coin(100_000_000, "C")))
-        .with_pool(2, (coin(100_000_000, "B"), coin(100_000_000, "C")))
+        .with_pool(1, (coin(100_000_000_000, "A"), coin(100_000_000_000, "C")))
+        .with_pool(2, (coin(100_000_000_000, "B"), coin(100_000_000_000, "C")))
         .build();
 
-    suite.deposit("alice", coin(100_000, "A")).unwrap();
-    suite.deposit("bob", coin(100_000, "B")).unwrap();
+    suite.deposit("alice", coin(100_000_000, "A")).unwrap();
+    suite.deposit("bob", coin(100_000_000, "B")).unwrap();
 
-    suite.borrow("alice", coin(65_000, "B")).unwrap(); // max loan
+    suite.borrow("alice", coin(65_000_000, "B")).unwrap(); // max loan
 
     // Bob is in the green, can't be liquidated yet
     suite
-        .liquidate("carol", "alice", "A", coin(65_000, "B"))
+        .liquidate("carol", "alice", "A", coin(65_000_000, "B"))
         .unwrap_err();
 
     // Put Bob under water, prime for liquidation
     suite
-        .swap_exact_in("bob", coin(1_000_000, "A"), "B")
+        .swap_exact_in("bob", coin(1_000_000_000, "A"), "B")
         .unwrap();
     assert_eq!(
         suite
@@ -39,46 +49,44 @@ fn liquidate_via_amm() {
             .validate(&Token::new_native("C"))
             .unwrap(),
         CreditLineValues {
-            collateral: Uint128::new(98_032),
-            credit_line: Uint128::new(63_720),
-            debt: Uint128::new(66_287),
+            collateral: Uint128::new(98_032_000),
+            credit_line: Uint128::new(63_720_000),
+            debt: Uint128::new(66_287_000),
         }
     );
 
     suite
         .liquidate("carol", "alice", "A", coin(65_000, "B"))
         .unwrap();
-    suite.reset_pools().unwrap();
-    assert_eq!(
-        suite
-            .query_total_credit_line("alice")
-            .unwrap()
-            .validate(&Token::new_native("C"))
-            .unwrap(),
-        CreditLineValues {
-            collateral: Uint128::new(35_000), // TODO: approximate, the exact value will be different
-            credit_line: Uint128::new(22_750), // TODO: approximate, the exact value will be different
-            debt: Uint128::zero(),
-        }
-    );
 
-    // Bob earns interest from liquidation since he's a B token lender
-    assert_eq!(
-        suite
-            .query_tokens_balance("B", "bob")
-            .unwrap()
-            .ltokens
-            .u128(),
-        100_000 + 2_925
-    );
-    // Carol earns a "trigger fee" from liquidation
+    // Carol earns a "trigger fee" from liquidation.
     assert_eq!(
         suite
             .query_tokens_balance("B", "carol")
             .unwrap()
             .ltokens
             .u128(),
-        325
+        325_000
+    );
+
+    // Bob earns interest from liquidation since he's a B token lender.
+    assert_eq!(
+        suite
+            .query_tokens_balance("B", "bob")
+            .unwrap()
+            .ltokens
+            .u128(),
+        100_000_000 + 2_925_000
+    );
+
+    // Bob sold some of his initial deposit to pay the debt.
+    assert!(
+        suite
+            .query_tokens_balance("A", "carol")
+            .unwrap()
+            .ltokens
+            .u128()
+            < 100_000_000
     );
 }
 
